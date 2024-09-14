388d5a5e5f294c7486b9bf91e3fda4573d5d3343
kubirthan <kubirthan7991@gmail.com>
2024-09-15 00:41:05 +0530

    creating item in list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Todo/>
     </div>
   );
--- src/Todo.js
+++ src/Todo.js
@@ -1,3 +1,49 @@
+import { useState } from "react"
+
 export default function Todo(){
-    return <h1>Todo components is work</h1>
+
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [todos, setTodos] = useState([])
+    const [error, setError] = useState("")
+    const [message, setMessage] = useState("")
+    const apiurl = "http://localhost:8000"
+
+    const handleSubmit = () => {
+        //check inputs
+        if(title.trim() !== '' && description.trim() !== '') {
+            fetch(apiurl+"/todos", {
+                method: 'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({title, description})
+            }).then((res) => {
+                if(res.ok) {
+                    //add item to list
+                    setTodos([...todos,{title, description}])
+                    setMessage("Item added successfully")
+                }else {
+                    //set error
+                    setError("Unable to create todo item")
+                }
+            })
+        }
+    }
+
+    return <>
+        <div className="row bg-success p-3 text-light">
+            <h1>Todo project with MERN stack</h1>
+        </div>
+        <div className="row">
+            <h3>Add Item</h3>
+            {message && <p className="text-success">{message}</p>}
+            <div className="form-group d-flex gap-2">
+                <input placeholder="Title" onChange={(e) => setTitle(e.target.value)} className="form-control" value={title} type="text"/>
+                <input placeholder="Description" onChange={(e) => setDescription(e.target.value)} className="form-control" value={description} type="text"/>
+                <button className="btn btn-dark" onClick={handleSubmit}>Submit</button>
+            </div>
+            {error && <p className="text-danger">{error}</p>}
+        </div>
+    </>
 }
