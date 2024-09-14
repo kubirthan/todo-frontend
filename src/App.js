300675d33dda7f1fcc6c46428e6dcaa042b8a373
kubirthan <kubirthan7991@gmail.com>
2024-09-14 22:29:54 +0530

    frontend setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './Todo';
+
+function App() {
+  return (
+    <div className="App">
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Todo.js
@@ -0,0 +1,3 @@
+export default function Todo(){
+    return <h1>Todo components is work</h1>
+}
